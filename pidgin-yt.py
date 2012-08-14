b017e4714cb830af2cc7ed60f07627fda2e9078f
Srikant <usp71084@gmail.com>
2012-08-14 21:21:35 +0530

    made modular and removed home page crash

--- pidgin-yt.py
+++ pidgin-yt.py
@@ -17,28 +17,28 @@ for root, dirnames, filenames in walk(path.expanduser('~/.mozilla/firefox/')):
                 matches.append(path.join(root, filename))
 
 
-"""Getting firefox status, sessionstore.js is continuously updated by firefox"""
 def readFirefoxTabs():
+    """Getting firefox status, sessionstore.js is continuously updated by firefox"""
     f = open(matches[0])
     firefoxData = json.load(f)
     return firefoxData['windows'][0]['tabs']
 
-    
-"""This will check for youtube in tabs and retrieve the video name from gdata's URL using JSON"""
+def findVideoID(eachURL):
+    url_data = urlparse.urlparse(eachURL)             
+    query = urlparse.parse_qs(url_data.query)
+    response = requests.get("http://gdata.youtube.com/feeds/api/videos/%s?v=2&alt=jsonc" %(query["v"][0]))
+    return json.loads(response.content)
+
 def updatePidgin():
+    """This will check for youtube in tabs and retrieve the video name from gdata's URL using JSON"""
     firefoxTabs = readFirefoxTabs()
     for someValue in xrange(len(firefoxTabs)):
         eachURL = str(firefoxTabs[someValue]['entries'][-1]['url'])
-        if "www.youtube.com" in eachURL and eachURL != previousYoutubeURL[0]:
-            url_data = urlparse.urlparse(eachURL)             
-            query = urlparse.parse_qs(url_data.query)
-            response = requests.get("http://gdata.youtube.com/feeds/api/videos/%s?v=2&alt=jsonc" %(query["v"][0]))
-            if(response.status_code == 200):                                                               
-                video_id = json.loads(response.content)
-                system("purple-remote 'setstatus?status=online&message=is watching: %s on youtube[%s]'"\
-                %(video_id['data']['title'],tinyurl.create_one(eachURL)))
-                previousYoutubeURL[0] = eachURL
-
+        if "www.youtube.com/watch" in eachURL and eachURL != previousYoutubeURL[0]:
+            video_id = findVideoID(eachURL)
+            system("purple-remote 'setstatus?status=online&message=watching: %s on youtube[%s](testing)'"\
+            %(video_id['data']['title'],tinyurl.create_one(eachURL)))
+            previousYoutubeURL[0] = eachURL
 
 
 if __name__=="__main__":
